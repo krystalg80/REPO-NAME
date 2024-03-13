aaa05bd9b76fbc17a8caac306b0b235794263760
krystalg80 <124218785+krystalg80@users.noreply.github.com>
2024-03-12 19:23:57 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Title from './Title.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Title />
+      <img src={logo} />
+    </div>
+  );
+}
+
+export default App;
+
+
